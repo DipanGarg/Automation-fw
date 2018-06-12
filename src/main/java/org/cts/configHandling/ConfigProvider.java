7b8cbbbf87e23a58d498e66044606936c95023a9
491073 <491073@PC382947.cts.com>
2018-06-12 13:20:34 +0530

    created src/test/resources folder

--- src/main/java/org/cts/configHandling/ConfigProvider.java
+++ src/main/java/org/cts/configHandling/ConfigProvider.java
@@ -1,54 +1,71 @@
 package org.cts.configHandling;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
-import java.util.UUID;
+
+import org.cts.utils.Constants;
 
 public final class ConfigProvider {
 
-	private static final String PROPERTIES_FILE = "//Resources//DBProperties.properties";
+	private static final String PROPERTIES_FILE = Constants.WORKING_DIR + "\\src\\test\\resources\\";
 	private static Properties props = new Properties();
-	public static final String WORKING_DIR=System.getProperty("user.dir");
-	public static final String EXTENT_REPORTS_FOLDER=WORKING_DIR+"\\Resources\\DBProperties.properties";
-	public static final String EXTENT_REPORTS_FOLDER1=WORKING_DIR+"\\Resources\\SeleniumProperties.properties";
-	
-	
-	// static method to get the instance of the ConfigProvider
-	
-/*	private ConfigProvider() {
-		  String ID = UUID.randomUUID().toString(); 
+
+	/**
+	 * static method to get the instance of the ConfigProvider 
+	 * @param propertyFileName
+	 * @return ConfigProvider
+	 * @throws FileNotFoundException
+	 */
+	public static ConfigProvider getInstance(String propertyFileName) throws FileNotFoundException {
+		final ConfigProvider configProvider = new ConfigProvider();
+		String path = PROPERTIES_FILE + propertyFileName + ".properties";
+		configProvider.loadProperties(path);
+		return configProvider;
 	}
-	*/
-	public static  ConfigProvider getInstance() throws FileNotFoundException {
+
+	/**
+	 * static method to get the instance of the ConfigProvider 
+	 * @return ConfigProvider
+	 * @throws FileNotFoundException
+	 */
+	public static ConfigProvider getInstance() throws FileNotFoundException {
 		final ConfigProvider configProvider = new ConfigProvider();
 		configProvider.loadProperties();
 		return configProvider;
 	}
-	
-	/*private InputStream getPropertiesFile() throws FileNotFoundException {
-		return new FileInputStream(EXTENT_REPORTS_FOLDER);
-		//	return this.getClass().getResourceAsStream(EXTENT_REPORTS_FOLDER);
-		
-	}*/
-	
+
 	private void loadProperties() {
 		try {
-			props.load(new FileInputStream(EXTENT_REPORTS_FOLDER));
-			props.load(new FileInputStream(EXTENT_REPORTS_FOLDER1));
-			} catch (Exception e) {
-			// TODO Auto-generated catch block
+			File folder = new File(PROPERTIES_FILE);
+			File[] listOfFiles = folder.listFiles();
+			for (File file : listOfFiles) {
+				if (file.isFile() && file.getName().endsWith(".properties")) {
+					props.load(new FileInputStream(folder + "\\" + file.getName()));
+				}
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-	
+
+	private void loadProperties(String propertyFile) {
+		try {
+			File file = new File(propertyFile);
+			if (file.isFile() && file.getName().endsWith(".properties")) {
+				props.load(new FileInputStream(file));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public String getProperty(String key) {
 		return props.getProperty(key);
 	}
-	
+
 	public void setProperty(String key, String value) {
 		props.setProperty(key, value);
 	}
---
+++ src/test/java/tests/ConfigTester.java
@@ -0,0 +1,21 @@
+package tests;
+
+
+
+import java.io.FileNotFoundException;
+
+import org.cts.configHandling.ConfigProvider;
+
+public class ConfigTester {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		   String hello = ConfigProvider.getInstance("SeleniumProperties").getProperty("dbpassword");
+		   //String hello1 = ConfigProvider.getInstance().getProperty("database");
+		   
+			//System.out.println("reading from configprovider\n"+ hello +"\n"+hello1);
+		   System.out.println("reading from configprovider\n"+ hello );
+
+	}
+
+}
